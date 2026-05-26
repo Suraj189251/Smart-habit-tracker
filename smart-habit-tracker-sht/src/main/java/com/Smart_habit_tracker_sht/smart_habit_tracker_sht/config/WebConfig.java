c634ff447c6b5ec5c67439c1ef08c17d6ab59e27
Suraj189251 <surajk3982@gmail.com>
2026-05-26 19:14:49 +0530

    Fixed backend folder issue

---
+++ smart-habit-tracker-sht/src/main/java/com/Smart_habit_tracker_sht/smart_habit_tracker_sht/config/WebConfig.java
@@ -0,0 +1,29 @@
+package com.Smart_habit_tracker_sht.smart_habit_tracker_sht.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+
+    @Value("${frontend.url}")
+    private String frontendurl;
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:5173", "http://localhost:5500")
+                       .allowedOriginPatterns(frontendurl)
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
